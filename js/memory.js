3e146ff6cb19d225d997d7c8342c9567b2132ac1
s.vogler@gmx.de <s.vogler@gmx.de>
2012-05-02 11:33:39 +0200

    initial commit

---
+++ js/memory.js
@@ -0,0 +1,184 @@
+var counter = 0;
+var numberOfPairs = 10;
+var numberOfChecked = 0;
+var pid_1;
+var pid_2;
+var products = [];
+var cardLaunch = false;
+
+function showGrid(query) {
+			
+    var url = 'http://svogler.inside-eu01.dw.demandware.net/s/Sites-SiteGenesis-Site/dw/shop/v12_2/product_search?q={query}&count=30&format=json&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&pretty_print=true'
+   	url = url.replace(/\{query\}/g, query);
+    
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        success: loadProductData        
+    });
+}
+
+function loadProductData(data) {
+	
+	if (data == null) {
+		showError('An error occured while communicating via OCAPI');
+		return false;
+	}
+	if (data.count < 10) {
+		showError('Your search returned ' + data.count + ' results. At least 10 results are required.');
+		return false;
+	}
+
+	var url = 'http://svogler.inside-eu01.dw.demandware.net/s/Sites-SiteGenesis-Site/dw/shop/v12_2/products/({ids})?format=json&client_id=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa&pretty_print=true'
+
+	var ids = [];
+	
+    // get all the ids and put it into an array
+    $.each(data.hits, function(index, value) {
+        ids.push(value.product_id);        
+    });
+    
+    shuffle(ids);
+    
+	url = url.replace(/\{ids\}/g, ids.slice(0,10).join("+"));
+	
+	$.ajax({
+	        url: url,
+	        dataType: 'jsonp',
+	        success: initGrid
+	});		
+	
+	$('#intro').fadeOut('slow', function() {
+		
+		$('.wrapper').fadeIn('slow', function() {
+			window.setTimeout(function(){
+				$('.memory section').addClass('flipped');
+				
+				window.setTimeout(function(){
+					$('.memory section').removeClass('flipped');
+					cardLaunch = true;
+				}, 3000);
+				
+			}, 1500);			
+		});
+
+	});
+	
+}
+
+function showError(message) {
+	$('#intro p.error').html(message).show('slow');
+}
+
+function shuffle(o) {
+    var j, x, i = o.length; 
+    while (i != 0) { 
+        j = parseInt(Math.random() * i);
+        x = o[--i];
+        o[i] = o[j];
+        o[j] = x;
+    }
+}
+
+function initGrid(data) {
+    
+    var images = [];
+    var grid = "";
+    
+    // store the result for later retrieval
+    products = data.data;
+    
+    // get all the products and put it into an array
+    $.each(products, function(i, product) {
+    	$.each(product.image_groups, function(j, group) {
+    		if (group.view_type == 'medium') {
+    			images.push({pid: product.id, image: group.images[0].link});
+    			return false; 
+    		}
+    	});        
+    });
+    
+    // double the content of the images array
+    $.merge( images, images );
+    
+    // shuffle the array
+    shuffle(images);
+    
+    
+    // get all the images and put it into an array
+    $.each(images, function(index, value) {
+    	var p = getProduct(value.pid);
+        grid = grid + "<section><figure class=\"back\"><img src=\"" + value.image + "\" data-pid=\"" + value.pid + "\" alt=\"\" title=\"\"/><div class=\"info name\"><span>" + p.name + "</span></div><div class=\"info price\"><span>Just " + p.price + " $</span></div></figure><figure class=\"front\"><img src=\"img/okapi.png\" alt=\"\" title=\"\"/></figure></section>";        
+    });    
+    
+    grid = grid + "<br clear=\"all\"/>";
+    $('.memory').html(grid);
+    
+    // get the number of pairs
+    numberOfPairs = images.length / 2;
+}
+
+
+function getProduct(pid) {
+	var p;
+	$.each(products, function(i, product) {
+   		if (product.id == pid) {
+   			p = product;
+   			return false; 
+   		}
+    });	
+	
+	return p;
+}
+
+
+function memoryCheck(pid_1, pid_2) {
+	counter=0;
+	
+	if(pid_1 == pid_2){
+		$('.memory section').each(function(){
+			if(($(this).find('img').attr('data-pid')== pid_1) || ($(this).find('img').attr('data-pid')== pid_2)){
+				$(this).removeClass('flipped').addClass('checked');
+			}
+		});
+        
+		numberOfChecked = ($('.memory .checked').length) / 2;		
+		
+		if(numberOfChecked == numberOfPairs) {
+			$('#couponcode').html('OKAPI2012');
+			$('.finished').fadeIn('slow');
+		}
+	} else {
+		$('.memory section').removeClass('flipped');
+	}
+}
+
+
+$(document).ready(function(){
+	
+	$("#submittopic").click(function() {
+			showError("");
+			var query = $('#inputtopic').val();
+            if (query != '') {
+            	showGrid(query);
+            }
+        });
+	
+	$('.memory').on('click', 'section', function(){
+        
+		if(($(this).attr('class') != 'flipped') && ($(this).attr('class') != 'checked') && (cardLaunch==true) && (counter < 2 )) {
+			$(this).addClass('flipped');
+			counter++;
+			if(counter == 1 ){
+				pid_1 = $(this).find('img').attr('data-pid');				
+			} else {
+				pid_2 = $(this).find('img').attr('data-pid');				
+				
+				window.setTimeout(function(){
+					memoryCheck(pid_1, pid_2);
+				}, 1000);
+			}
+		}
+	});
+	
+});
